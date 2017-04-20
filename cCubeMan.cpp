2d40a86bb3f6d96b806af314a2aaa92357a546fa
Yoo JiSang <digndign2@naver.com>
2017-04-20 11:00:31 +0900

    CubeMan의 포인터 NULL 초기화

---
+++ cCubeMan.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "cCubeMan.h"
+
+
+cCubeMan::cCubeMan()
+	: m_pHead(NULL), m_pBody(NULL), m_pLeftArm(NULL), m_pLeftLeg(NULL), m_pRightArm(NULL), m_pRightLeg(NULL)
+{
+}
+
+
+cCubeMan::~cCubeMan()
+{
+}
+
+void cCubeMan::Setup()
+{
+}
+
+void cCubeMan::Update()
+{
+}
+
+void cCubeMan::Render()
+{
+}
